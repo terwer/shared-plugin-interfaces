a96b674345588e6c99be99e3d29fd042a4d9df3f
Terwer <youweics@sina.com>
2018-11-30 19:02:07 +0800

    反射参数类型处理

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ mvn package -DskipTests
 2. 安装到本地maven库
 
 ```
-mvn install:install-file -Dfile=target/shared-plugin-interfaces-3.2.1.jar -DgroupId=com.terwergreen -DartifactId=shared-plugin-interfaces -Dversion=3.2.1 -Dpackaging=jar
+mvn install:install-file -Dfile=target/shared-plugin-interfaces-3.2.2.jar -DgroupId=com.terwergreen -DartifactId=shared-plugin-interfaces -Dversion=3.2.2 -Dpackaging=jar
 ```
 
 #### 使用说明
@@ -29,7 +29,7 @@ mvn install:install-file -Dfile=target/shared-plugin-interfaces-3.2.1.jar -Dgrou
 <dependency>
     <groupId>com.terwergreen</groupId>
     <artifactId>shared-plugin-interfaces</artifactId>
-    <version>3.2.1</version>
+    <version>3.2.2</version>
 </dependency>
 ```
 
--- src/main/java/com/terwergreen/util/ReflectUtil.java
+++ src/main/java/com/terwergreen/util/ReflectUtil.java
@@ -78,12 +78,38 @@ public class ReflectUtil {
     public static Object newInstance(Class<?> clazz, Object... initargs) {
         Constructor<?> constructor = null;
         try {
-            Class[] argTypes = new Class[initargs.length];
+            Class[] initargTypes = new Class[initargs.length];
             for (int i = 0; i < initargs.length; i++) {
                 Object initarg = initargs[i];
-                argTypes[i] = initarg.getClass();
+                initargTypes[i] = initarg.getClass();
             }
-            constructor = clazz.getDeclaredConstructor(argTypes);
+            constructor = clazz.getDeclaredConstructor(initargTypes);
+            //设置允许访问，防止private修饰的构造方法
+            constructor.setAccessible(true);
+            return constructor.newInstance(initargs);
+        } catch (NoSuchMethodException e) {
+            logger.error("Class " + clazz + "newInstance fail", e);
+        } catch (IllegalAccessException e) {
+            logger.error("Class " + clazz + "newInstance fail", e);
+        } catch (InstantiationException e) {
+            logger.error("Class " + clazz + "newInstance fail", e);
+        } catch (InvocationTargetException e) {
+            logger.error("Class " + clazz + "newInstance fail", e);
+        }
+        return null;
+    }
+
+    /**
+     * 反射读取构造方法创建新对象，自定义所有参数类型
+     *
+     * @param clazz
+     * @param initargs 构造函数参数
+     * @return
+     */
+    public static Object newInstance(Class<?> clazz, Class[] initargTypes, Object[] initargs) {
+        Constructor<?> constructor = null;
+        try {
+            constructor = clazz.getDeclaredConstructor(initargTypes);
             //设置允许访问，防止private修饰的构造方法
             constructor.setAccessible(true);
             return constructor.newInstance(initargs);
@@ -177,6 +203,30 @@ public class ReflectUtil {
                 Object arg = args[i];
                 argTypes[i] = arg.getClass();
             }
+            Method method = clazz.getDeclaredMethod(methodName, argTypes);
+            method.setAccessible(true);//为true则表示反射的对象在使用时取消Java语言访问检查
+            return method.invoke(obj, args);
+        } catch (NoSuchMethodException e) {
+            logger.error("Class " + obj + " invoke function" + methodName + " fail", e);
+        } catch (IllegalAccessException e) {
+            logger.error("Class " + obj + " invoke function" + methodName + " fail", e);
+        } catch (InvocationTargetException e) {
+            logger.error("Class " + obj + " invoke function" + methodName + " fail", e);
+        }
+        return null;
+    }
+
+    /**
+     * 反射调用有参方法，自定义所有参数类型
+     *
+     * @param obj
+     * @param methodName
+     * @param args
+     * @return
+     */
+    public static Object invoke(Object obj, String methodName, Class[] argTypes, Object[] args) {
+        try {
+            Class<?> clazz = obj.getClass();
             Method method = clazz.getDeclaredMethod(methodName, argTypes);
             method.setAccessible(true);//为true则表示反射的对象在使用时取消Java语言访问检查
             return method.invoke(obj, args);
