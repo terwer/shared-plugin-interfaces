0bc705189c86bf88e2bfe5d1fdf19bc9c3b2828e
Terwer <youweics@sina.com>
2020-12-10 18:22:06 +0800

    调整构建脚本

---
+++ install.sh
@@ -0,0 +1,8 @@
+echo 'current workspace:'
+echo $WORKSPACE
+cd $WORKSPACE
+chmod +x mvnw
+#  构建jar包
+./mvnw clean package -DskipTests
+# 安装到本地maven库
+./mvnw install:install-file -Dfile=target/shared-plugin-interfaces-4.1.13.jar -DgroupId=com.terwergreen -DartifactId=shared-plugin-interfaces -Dversion=4.1.13 -Dpackaging=jar
